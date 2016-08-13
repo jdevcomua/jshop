75bd990297db919de29c32f263808dddcbd8d9e3
umka <litvinova.a95@gmail.com>
2016-08-14 00:27:19 +0300

    autocomplete

--- common/components/autocomplete.php
+++ common/components/autocomplete.php
@@ -1,6 +1,21 @@
 <?php
 
+use yii\web\Application;
+
 class Yii extends \yii\BaseYii
+{
+    /**@var MyApp*/
+    static $app;
+}
+
+/**
+ * Class MyApp
+ *
+ * @property \common\components\Cart $cart
+ * @property \common\components\UrlHelper $urlHelper
+ * @property \common\components\Compare $compare
+ */
+class MyApp extends Application
 {
 
 }

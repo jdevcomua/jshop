383a67b01a5d97d53e74a46d12403a872d37d84f
Максим Гордиенко <mgordienko@iseck.com>
2017-07-06 01:18:33 +0300

    delete views

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -5,4 +5,7 @@ use common\components\Theme;
 return [
     'adminEmail'    => 'admin@example.com',
     'theme'         => Theme::TEMPLATE_BASIC,
+    'themeParams' => [
+        Theme::PARAM_ITEMS_ON_FIRST_PAGE => 9,
+    ],
 ];

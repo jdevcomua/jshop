43821db311404f086eb407c2dbbdb313c5c04729
Maxim Gordienko <3dmaxpayne@gmail.com>
2019-04-20 15:57:58 +0300

    move web from backend to frontend

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,14 +3,26 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=freemark_sdeldn',
+            'username' => 'freemark_sdeldn',
+            'password' => '4eTe3S~8v^',
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'mail.adm.tools',
+                'username' => 'info@sdelivery.dn.ua',
+                'password' => 'MexD8X5l6Xh2',
+                'port' => '465',
+                'encryption' => 'ssl',
+            ],
         ],
     ],
 ];

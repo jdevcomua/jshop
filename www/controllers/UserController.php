ed272f9065b7e96bb6e5b5ae8b9ffb84769fd20c
Petro Tkachuk <tkachuk.petro.r@gmail.com>
2019-04-24 21:52:53 +0300

    #9544 Авторизация через fb fix test
    Signed-off-by: Petro Tkachuk <tkachuk.petro.r@gmail.com>

--- www/controllers/UserController.php
+++ www/controllers/UserController.php
@@ -81,9 +81,6 @@ class UserController extends Controller
 
     public function actionFacebookAuth()
     {
-        if (!Yii::$app->session->isActive) {
-            Yii::$app->session->open();
-        }
         $fb = new Facebook([
             'app_id' => Yii::$app->params['fbAppId'],
             'app_secret' => Yii::$app->params['fbSecretKey'],
@@ -92,10 +89,6 @@ class UserController extends Controller
         $helper = $fb->getRedirectLoginHelper();
         try {
             $accessToken = $helper->getAccessToken();
-            $response = $fb->get('/me',array (
-                'fields' => 'id','email','name'
-            ), $accessToken);
-            $userNode = $response->getGraphUser();
         } catch(FacebookResponseException $e) {
             var_dump('1' . $e->getMessage() );
             exit;
@@ -103,6 +96,24 @@ class UserController extends Controller
             var_dump('2' . $e->getMessage());
             exit;
         }
+        $_SESSION['fb_access_token'] = (string) $accessToken;
+
+        try {
+            // Get the \Facebook\GraphNodes\GraphUser object for the current user.
+            // If you provided a 'default_access_token', the '{access-token}' is optional.
+            $response = $fb->get('/me?fields=id,name,email,first_name,last_name,gender', $_SESSION['fb_access_token']);
+        } catch (\Facebook\Exceptions\FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+
+        $userNode = $response->getGraphUser();
+
         if (isset($userNode)) {
             var_dump($userNode);
             exit;

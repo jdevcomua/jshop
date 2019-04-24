36e79c41afaeb3bd90dfda42e8f6a3d4e056f691
Petro Tkachuk <tkachuk.petro.r@gmail.com>
2019-04-24 15:45:09 +0300

    #9544 Авторизация через fb fix test
    Signed-off-by: Petro Tkachuk <tkachuk.petro.r@gmail.com>

--- www/controllers/UserController.php
+++ www/controllers/UserController.php
@@ -96,12 +96,28 @@ class UserController extends Controller
             $userNode = $response->getGraphUser();
         } catch(FacebookResponseException $e) {
             echo 'Graph returned an error: ' . $e->getMessage();
-            exit;
+//            exit;
         } catch(FacebookSDKException $e) {
             echo 'Facebook SDK returned an error: ' . $e->getMessage();
+//            exit;
+        }
+
+        if (! isset($accessToken)) {
+            if ($helper->getError()) {
+                header('HTTP/1.0 401 Unauthorized');
+                echo "Error: " . $helper->getError() . "\n";
+                echo "Error Code: " . $helper->getErrorCode() . "\n";
+                echo "Error Reason: " . $helper->getErrorReason() . "\n";
+                echo "Error Description: " . $helper->getErrorDescription() . "\n";
+            } else {
+                header('HTTP/1.0 400 Bad Request');
+                echo 'Bad request';
+            }
             exit;
         }
 
+        // Logged in
+        var_dump($accessToken->getValue());
         $user = User::find()->andFilterWhere(['fb_id' => $userNode->getId()])->one();
         if (empty($user)) {
             $user = new User();

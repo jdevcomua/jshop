f270e41c02dac69b34cba7ab47c23d9245b118d4
Maxim Gordienko <3dmaxpayne@gmail.com>
2019-05-20 00:21:15 +0300

    add dg function

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,10 @@ Yii::setAlias('www', dirname(dirname(__DIR__)) . '/www');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('rest', dirname(dirname(__DIR__)) . '/rest');
+
+function dg($var, $isDie = true) {
+    \yii\helpers\VarDumper::dump($var, 10, true);
+    if ($isDie) {
+        die;
+    }
+}

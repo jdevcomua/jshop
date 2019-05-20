3ab1c8b4744b57ba159b78a7ac065044f5257094
Ihor Bielovol <bielovol.ihor@jdev.com.ua>
2019-05-20 11:01:46 +0300

    #9754 Сделать пункт меню Каталог

--- www/themes/babyshop/widgets/category/views/menu.php
+++ www/themes/babyshop/widgets/category/views/menu.php
@@ -9,16 +9,11 @@ use yii\helpers\Url;
 /* @var ItemCat $child_child */
 /* @var ItemCat $category */
 /* @var $inCategory boolean */
+
 ?>
+<li class="mega-menu"> <a class="level-top" href="" ><span><?= Yii::t('app','Categories') ?></span></a>
 
- <?php foreach ($allCategories as $category) {
-     $children = $category->getChildren()->all();;
-     if (empty($children))
-         echo '<li class="mega-menu"> 
-            <a class="level-top" href="' . $category->getUrl()  . '"><span>' . $category->title  . '</span></a> </li>';
-     else {?>
 
-<li class="mega-menu"> <a class="level-top" href="<?= $category->getUrl() ?>"><span><?= $category->title ?></span></a>
     <div class="level0-wrapper dropdown-6col">
         <div class="container">
             <div class="level0-wrapper2">
@@ -26,31 +21,17 @@ use yii\helpers\Url;
                     <div class="nav-block nav-block-center">
                         <!--mega menu-->
                         <ul class="level0">
-                            <?php foreach ($children as $child) {
-                                $child_children = $child->getChildren()->all()?>
-                            <li class="level3 nav-6-1 parent item"> <a href="<?= $child->getUrl() ?>"><span><?= $child->title ?></span></a>
+                            <?php foreach ($allCategories as $category): ?>
+                            <li class="level3 nav-6-1 parent item"> <a href="<?= $category->getUrl() ?>"><span><?= $category->title ?></span></a>
                                 <!--sub sub category-->
-                                <?php if(!empty($child_children)) {?>
-                                <ul class="level1">
-                                    <?php foreach ($child_children as $child_child) { ?>
-                                    <li class="level2 nav-6-1-1"> <a href="<?= $child_child->getUrl() ?>"><span><?= $child_child->title ?></span></a> </li>
-                                    <!--level2 nav-6-1-1-->
-                                   <?php } ?>
-                                </ul>
-                                <?php }?>
                                 <!--level1-->
                                 <!--sub sub category-->
                             </li>
-                            <?php }?>
+                            <?php endforeach;?>
                         </ul>
                         <!--level0-->
                     </div>
                     <!--nav-block nav-block-center-->
-                </div>
-                <!--col-1-->
-                <div class="col-2">
-                    <div class="menu_image"><a title="" href="<?= $category->getUrl() ?>">
-                            <img alt="menu_image" src="<?= ($category->image) ? $category->image : '/images/category_no_image.jpg'?>"></a></div>
                 </div>
                 <!--col-2-->
             </div>
@@ -62,4 +43,4 @@ use yii\helpers\Url;
     <!--mega menu-->
 </li>
 
-<?php } }?>
+

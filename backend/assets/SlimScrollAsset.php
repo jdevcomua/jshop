6d9bf04f2f1e8fcd7dccefdcfd29aaa7bb41b4dc
Ihor Bielovol <bielovol.ihor@jdev.com.ua>
2019-05-31 15:29:43 +0300

    Рефакторинг

--- backend/assets/SlimScrollAsset.php
+++ backend/assets/SlimScrollAsset.php
@@ -9,17 +9,14 @@ namespace backend\assets;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
+
 class SlimScrollAsset extends AssetBundle
 {
     public $sourcePath = '@vendor/grimmlink/jquery-slimscroll';
 
-//    public $baseUrl = '@web';
+
     public $js = [
         'jquery.slimscroll.min.js',
-//        'js/onClickFunctions.js'
+
     ];
 }

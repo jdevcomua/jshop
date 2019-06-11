77aa3a1ee349c114a7640fd9d050d6e309033da1
Ihor Bielovol <bielovol.ihor@jdev.com.ua>
2019-06-11 10:30:13 +0300

    #9876 Social networks at footer

--- www/themes/babyshop/views/item/item.php
+++ www/themes/babyshop/views/item/item.php
@@ -118,14 +118,10 @@ $imageUrls = $item->getImageUrl();
                                     <p class="email-friend"><a href="#" class=""><span><?= Yii::t('app','Email to a Friend')?></span></a></p>
                                 </div>
                                 <div class="social">
-                                    <ul class="link">
+                                    <ul>
                                         <li class="fb"><a href="#"></a></li>
-                                        <li class="tw"><a href="#"></a></li>
-                                        <li class="googleplus"><a href="#"></a></li>
-                                        <li class="rss"><a href="#"></a></li>
-                                        <li class="pintrest"><a href="#"></a></li>
-                                        <li class="linkedin"><a href="#"></a></li>
-                                        <li class="youtube"><a href="#"></a></li>
+                                        <li class="instagram"><a href="#"></a></li>
+                                        <li class="viber-grey"><a href="#"></a></li>
                                     </ul>
                                 </div>
                             </div>
--- www/themes/babyshop/views/layouts/footer.php
+++ www/themes/babyshop/views/layouts/footer.php
@@ -98,12 +98,8 @@
                     <div class="social">
                         <ul>
                             <li class="fb"><a href="#"></a></li>
-                            <li class="tw"><a href="#"></a></li>
-                            <li class="googleplus"><a href="#"></a></li>
-                            <li class="rss"><a href="#"></a></li>
-                            <li class="pintrest"><a href="#"></a></li>
-                            <li class="linkedin"><a href="#"></a></li>
-                            <li class="youtube"><a href="#"></a></li>
+                            <li class="instagram"><a href="#"></a></li>
+                            <li class="viber"><a href="#"></a></li>
                         </ul>
                     </div>
                 </div>

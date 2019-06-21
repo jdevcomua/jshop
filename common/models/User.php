e3ee77649bc61bcc4e03c73ea32ca4f1b8383132
Ihor Bielovol <bielovol.ihor@jdev.com.ua>
2019-06-21 18:22:39 +0300

    fix bugs

--- common/models/User.php
+++ common/models/User.php
@@ -70,7 +70,7 @@ class User extends Model implements \yii\web\IdentityInterface
             [['email'], 'trim'],// обрезает пробелы вокруг "email"
             [['password'], 'string', 'length' => [6, 25]],
             [['name', 'surname', 'address', 'phone', 'access_token', 'password','city'], 'string'],
-            [['address','name','surname'],'length' => [0, 50]],
+            [['address','name','surname'],'string','length' => [0, 50]],
             [['email'], 'email'],
         ];
     }

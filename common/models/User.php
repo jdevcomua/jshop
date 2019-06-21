5af7d1fc1ef11a83309fd718f67dee59d8b2ed3a
Ihor Bielovol <bielovol.ihor@jdev.com.ua>
2019-06-21 18:21:04 +0300

    fix bugs

--- common/models/User.php
+++ common/models/User.php
@@ -68,9 +68,9 @@ class User extends Model implements \yii\web\IdentityInterface
             ['password', 'compare', 'compareAttribute' => 'confirm_password', 'operator' => '==', 'on' => 'adminPass'],
             [['email', 'vk_id', 'fb_id'], 'unique'],
             [['email'], 'trim'],// обрезает пробелы вокруг "email"
-            [['address','name','surname'],'length' => [0, 50]],
             [['password'], 'string', 'length' => [6, 25]],
             [['name', 'surname', 'address', 'phone', 'access_token', 'password','city'], 'string'],
+            [['address','name','surname'],'length' => [0, 50]],
             [['email'], 'email'],
         ];
     }

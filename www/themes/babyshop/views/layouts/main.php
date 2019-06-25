2a7c7c7af200d017f0c0e43cebba5e069c523ec0
Ihor Bielovol <bielovol.ihor@jdev.com.ua>
2019-06-25 13:46:17 +0300

    #10038 Рефакторинг

--- www/themes/babyshop/views/layouts/main.php
+++ www/themes/babyshop/views/layouts/main.php
@@ -156,43 +156,10 @@ use yii\widgets\Pjax;
             </div>
         </div>
         <?php Pjax::end() ?>
-
+        <div class="down"></div>
     </div>
 
-<div class="container">
-    <div class="row our-features-box">
-        <ul>
-            <li>
-                <div class="feature-box">
-                    <div class="icon-truck"></div>
-                    <div class="content"><?= Yii::t('app','FREE SHOPPING on order over $99')?></div>
-                </div>
-            </li>
-            <li>
-                <div class="feature-box">
-                    <div class="icon-support"></div>
-                    <div class="content"><?= Yii::t('app','Have a question?')?><br>
-                        +1 800 789 0000</div>
-                </div>
-            </li>
-            <li>
-                <div class="feature-box">
-                    <div class="icon-money"></div>
-                    <div class="content">100% <?= Yii::t('app','Money Back Guarantee')?></div>
-                </div>
-            </li>
-            <li>
-                <div class="feature-box">
-                    <div class="icon-return"></div>
-                    <div class="content"><?= Yii::t('app','30 days return Service')?></div>
-                </div>
-            </li>
-            <li class="last">
-                <div class="feature-box"> <a href="#"><i class="fa fa-apple"></i> <?= Yii::t('app','download')?></a> <a href="#"><i class="fa fa-android"></i> <?= Yii::t('app','download')?></a> </div>
-            </li>
-        </ul>
-    </div>
-</div>
+
 
 <?= $this->render('footer') ?>
 <!-- End For version 1,2,3,4,6 -->

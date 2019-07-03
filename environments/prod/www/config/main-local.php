88d4435a56beb391aaed77893696a7b984d5ba70
Ihor Bielovol <bielovol.ihor@jdev.com.ua>
2019-07-03 18:16:54 +0300

    mailer

--- environments/prod/www/config/main-local.php
+++ environments/prod/www/config/main-local.php
@@ -1,6 +1,26 @@
 <?php
 return [
     'components' => [
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'useFileTransport'=>false,
+            'transport'=>[
+                'class'=>'Swift_SmtpTransport',
+                'host'=>'mail.adm.tools',
+                'username'=>'info@sdelivery.dn.ua',
+                'password'=>'MexD8X5l6Xh2',
+                'port'=>'465',
+                'encryption'=>'ssl',
+                'streamOptions' => [
+                    'ssl' => [
+                        'allow_self_signed' => true,
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                    ],
+                ]
+            ],
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',

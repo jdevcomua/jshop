b1a70cc7ee015a49afc521b572031f463d89f29b
Ihor Bielovol <bielovol.ihor@jdev.com.ua>
2019-08-13 13:36:21 +0300

    mailer

--- www/config/params.php
+++ www/config/params.php
@@ -3,7 +3,7 @@ use common\components\Theme;
 
 return [
     'adminEmail'    => 'info@sdelivery.dn.ua',
-    'orderEmail'    => 'admin@example.com',
+    'orderEmail'    => 'order@sdelivery.dn.ua',
     'fbAppId' => '1278586458955080',
     'fbSecretKey' => '9025f7e15e75980534f81a9dcbacd121',
     'theme'         => Theme::TEMPLATE_BABYSHOP,

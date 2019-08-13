a89047593598b88c0da32c57b051c0abce5bc022
Ihor Bielovol <bielovol.ihor@jdev.com.ua>
2019-08-13 16:57:11 +0300

    #10216

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -26,7 +26,7 @@ use yii\widgets\ActiveForm;
 
     echo $form->field($model, 'fb_id')->textInput(['maxlength' => true]);
 
-    echo $form->field($model, 'password')->textInput(['maxlength' => true]);
+    echo $form->field($model, 'password')->textInput(['maxlength' => true, 'required'=> true]);
 
     echo $form->field($model, 'confirm_password')->textInput(['maxlength' => true]);
     ?>

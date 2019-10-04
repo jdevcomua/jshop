3ba356ce6c3d0c6fd6d2004cf13063f8c7ae8e90
Ihor Bielovol <bielovol.ihor@jdev.com.ua>
2019-10-04 16:00:16 +0300

    #10883 Data display

--- www/views/item/_item_comment.php
+++ www/views/item/_item_comment.php
@@ -12,7 +12,7 @@
             </div>
             <p class="meta">
                 <strong><?= ($model->user) ? $model->user->name : 'Anonymous' ?></strong>
-                <span>–</span> <?= Yii::$app->formatter->asDatetime($model->timestamp,'dd/MM/yy   H:i') ?>
+                <span>–</span> <?= Yii::$app->formatter->asDatetime($model->timestamp,'dd/MM/yy H:i') ?>
             </p>
             <div class="description">
                 <?= $model->text ?>
